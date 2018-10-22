a67b05b148caf422335c6c3cb513f297b1073c95
Collin Moon <mooncollin@gmail.com>
2018-10-22 15:04:46 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/events')
+def events():
+	return render_template('events.html')
