e9283d06d3778d5bfcb284e5b724a174efdb2eda
Collin Moon <mooncollin@gmail.com>
2019-01-02 19:19:42 -0600

    removed unnecessary files and imports

--- auth/auth.py
+++ auth/auth.py
@@ -1,5 +1,5 @@
 from flask import (
-	render_template, redirect, url_for, session
+	redirect, url_for, session
 )
 from functools import wraps
 
--- database/models/main.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from database.sqldb import db as db
-
-# class Event(db.Model):
-# 	pass
